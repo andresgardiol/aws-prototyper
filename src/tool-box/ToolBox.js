bd42c265281b329de1db1b2aff0dd7cd213fd24c
Andres Gardiol <andresgardiol@gmail.com>
2021-07-27 01:01:33 -0300

    Separate style from component

--- src/tool-box/ToolBox.js
+++ src/tool-box/ToolBox.js
@@ -4,6 +4,7 @@ import clsx from "clsx";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import {AddNodeTool} from "./tools/AddNodeTool";
+import {useStyles} from "./styles";
 
 export function ToolBox({onClickAddNode}) {
     const classes = useStyles();
@@ -38,38 +39,3 @@ export function ToolBox({onClickAddNode}) {
         </>
     )
 }
-
-
-const drawerWidth = 200;
-const useStyles = makeStyles((theme) => ({
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-    },
-    drawerOpen: {
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    drawerClose: {
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        overflowX: 'hidden',
-        width: theme.spacing(7) + 1,
-        [theme.breakpoints.up('sm')]: {
-            width: theme.spacing(7) + 1,
-        },
-    },
-    toolbar: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: theme.spacing(0, 1),
-        ...theme.mixins.toolbar,
-    }
-}));
